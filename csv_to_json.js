7e7cfe904e6bef5d4883d63c8bf05ca7c96a3ecf
sendmenas <giedrius.bielskis@gmail.com>
2018-03-31 09:58:44 +0300

    Initial commit

---
+++ csv_to_json.js
@@ -0,0 +1,24 @@
+// Solution guide http://www.techniche.co/insights/reading-excel-sheet-data-using-node-xlsx/
+
+var xlsx = require('node-xlsx');
+const fs = require('fs')
+const path = require('path')
+
+const xlsxObj = xlsx.parse('./customer-data.csv');
+const header = xlsxObj[0].data[0];
+const data = xlsxObj[0].data;
+const jsonArr = []
+
+for (var i = 1; i < data.length; i++) {
+	var contactObj = {};
+	for (var x = 0; x < data[i].length; x++) {
+		contactObj[header[x]] = data[i][x];
+	}
+	jsonArr.push(contactObj);
+}
+
+const content = JSON.stringify(jsonArr);
+
+fs.writeFile("data.json", content);
+
+console.log("completed");
